b768f66e0722d2b76bfecb2ce4af8e392a0b7fd3
AlyonaBasko <33734752+AlyonaBasko@users.noreply.github.com>
2024-03-31 22:01:03 +0200

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,28 +1,61 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
+const inputDatePicker = document.getElementById("datetime-picker");
+const startButton = document.querySelector('[data-start]');
+const daysElement = document.querySelector('[data-days]');
+const hoursElement = document.querySelector('[data-hours]');
+const minutesElement = document.querySelector('[data-minutes]');
+const secondsElement = document.querySelector('[data-seconds]');
 
-// flatpickr("#datetimePicker", {
-//     enableTime: true, // дозволяє вибрати час
-//     dateFormat: "Y-m-d H:i", // формат дати і часу
-//   });
+startButton.disabled = true;
 
-  let userSelectedDate;
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    currentDifferenceDate(selectedDates[0]);
+  },
+};
 
-  const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-        const selectedDate = selectedDates[0];
-        console.log("Обрана дата:", selectedDate);
-    },
-    if (selectedDate >= new Date()) {
-        userSelectedDate = selectedDate;
-      } else {
-        console.log("Обрана дата не може бути у минулому!");
-      },
-  };
+const datetimePicker = flatpickr(inputDatePicker, options);
 
-  flatpickr("#datetimePicker", options);
+function currentDifferenceDate(selectedDate) {
+  const currentDate = Date.now();
+  startButton.disabled = selectedDate < currentDate;
+  if (selectedDate < currentDate) {
+    iziToast.error({message: 'Please choose a date in the future'});
+  } else {
+    inputDatePicker.disabled = true;
+    startTimer(selectedDate - currentDate);
+  }
+}
+
+function startTimer(duration) {
+  let timer = duration / 1000;
+  const intervalId = setInterval(function () {
+    const days = Math.floor(timer / (3600 * 24));
+    const hours = Math.floor((timer % (3600 * 24)) / 3600);
+    const minutes = Math.floor((timer % 3600) / 60);
+    const seconds = Math.floor(timer % 60);
+    renderDate(days, hours, minutes, seconds);
+    if (--timer < 0) {
+      clearInterval(intervalId);
+      renderDate(0, 0, 0, 0);
+      iziToast.success({message: 'Countdown finished!'});
+      inputDatePicker.disabled = false;
+    }
+  }, 1000);
+}
+
+function renderDate(days, hours, minutes, seconds) {
+  daysElement.textContent = days.toString().padStart(2, '0');
+  hoursElement.textContent = hours.toString().padStart(2, '0');
+  minutesElement.textContent = minutes.toString().padStart(2, '0');
+  secondsElement.textContent = seconds.toString().padStart(2, '0');
+}
