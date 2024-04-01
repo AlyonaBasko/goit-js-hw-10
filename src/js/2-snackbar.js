61d365cb66bbbd32513b820a1d82f21d1efad871
AlyonaBasko <33734752+AlyonaBasko@users.noreply.github.com>
2024-04-01 22:50:18 +0200

    Update 2-snackbar.js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,23 +1,24 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-const form = document.querySelector('form');
-const delayInput = document.getElementById('delay');
-const successRadio = document.getElementById('success');
-const failureRadio = document.getElementById('failure');
-
+const form = document.querySelector('.form');
 
 form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
+
+  const delayInput = document.querySelector('input[name="delay"]');
+  const stateInput = document.querySelector('input[name="state"]:checked');
   
   const delay = parseInt(delayInput.value);
+  const state = stateInput.value;
+
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (successRadio.checked) {
+      if (state === 'fulfilled') {
         resolve(delay);
-      } else if (failureRadio.checked) {
+      } else if (state === 'rejected') {
         reject(delay);
       }
     }, delay);
@@ -25,10 +26,16 @@ function handleSubmit(event) {
 
   promise.then(
     (delay) => {
-      iziToast.success({ message: `✅ ${successRadio.value} notification in ${delay}ms` });
+      iziToast.success({
+        message: `✅ Notification created after ${delay}ms`,
+        position: 'topLeft'
+      });
     },
     (delay) => {
-      iziToast.error({ message: `❌ ${failureRadio.value} notification in ${delay}ms` });
+      iziToast.error({
+        message: `❌ Notification failed after ${delay}ms`,
+        position: 'topLeft'
+      });
     }
   );
 }
